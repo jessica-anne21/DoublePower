a0c3cc148af48e23414ad268b98850d3233620e8
SelinVelday <2272048@maranatha.ac.id>
2024-05-23 18:21:13 +0700

    adding files

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.ENUM('administrator', 'mahasiswa', 'program_studi', 'fakultas'),
+        allowNull: false
+    },
+    program_studi_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    fakultas_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    }
+});
+
+module.exports = User;
